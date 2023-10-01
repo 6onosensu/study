5c3c1d087e6915d218b46a1dd94fb58bb5ffd4e1
Dasha <darja.portnova@gmail.com>
2023-10-01 22:06:49 +0300

    functions

--- book/functions.js
+++ book/functions.js
@@ -35,6 +35,7 @@ function greet(who) {
 greet("Harry");
 console.log("Bye!");
 
+
 // Optional arguments
 console.log(square2(4, true, "hedgehog"));
 //=> 16
@@ -44,3 +45,55 @@ function minus(a, b) {
   else return a - b;
 }
 console.log(minus(10) + " " + minus(10, 5));
+
+function power1(base, exponent = 2) {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+}
+console.log(power1(4) + " " + power1(2, 6));
+
+
+//  Замыкание - Closure
+function wrapValue(n) {
+  let local = n;
+  return () => local;
+}
+let wrap1 = wrapValue(1);
+let wrap2 = wrapValue(2);
+console.log(wrap1());
+console.log(wrap2());
+
+function multiplier(factor) {
+  return number => number * factor;
+}
+let twice = multiplier(2);
+console.log(twice(5));
+
+
+//Recursion
+function power3(base, exponent) {
+  if (exponent == 0) {
+    return 1;
+  } else {
+    return base * power3(base, exponent - 1);
+  }
+}
+console.log(power(2, 3));
+
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) {
+      return history;
+    } else if (current > target) {
+      return null;
+    } else {
+      return find(current + 5, `(${history} + 5)`) ||
+        find(current * 3, `(${history} * 3)`);
+    }
+  }
+  return find(1, "1");
+}
+console.log(findSolution(24));
