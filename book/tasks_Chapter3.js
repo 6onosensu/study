4d9b7abdf7478f937de872b240d1fa9db793a37f
Dasha <darja.portnova@gmail.com>
2023-10-02 21:49:15 +0300

    added function minimum of the numbers

---
+++ book/tasks_Chapter3.js
@@ -0,0 +1,12 @@
+//Minimum
+let min = (a, b) => Math.min(a, b);
+console.log(min(5, 4));
+
+function min1(a, b) {
+  if (a < b) return a;
+  else return b;
+}
+console.log(min1(9, 2));
+
+
+//Recursion
