7bb2574eb4caa92e0953ced22ae95038baa2e6f2
Dasha <darja.portnova@gmail.com>
2023-10-03 12:40:18 +0300

    tasks completed (chapter 3)

--- book/tasks_Chapter3.js
+++ book/tasks_Chapter3.js
@@ -10,3 +10,28 @@ console.log(min1(9, 2));
 
 
 //Recursion
+function isEven(n) {
+  if (n == 0) return true;
+  else if (n == 1) return false;
+  else if (n < 0) return isEven(-n);
+  else return isEven(n - 2);
+}
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+
+
+//Letter counting
+function countChar(string, ch) {
+  let counter = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == ch) counter++;
+  }
+  return counter;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
+console.log(countBs("BBC"));
+console.log(countChar("kakkerlak", "k"));
